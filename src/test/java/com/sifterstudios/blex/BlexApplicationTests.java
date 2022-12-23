928ab9744e8971b31ba73ec6f607e101cadbe0ad
Tormod Liseth <tormod@sifterstudios.no>
2022-12-23 01:36:23 +0100

    added sveltekit

---
+++ src/test/java/com/sifterstudios/blex/BlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sifterstudios.blex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
