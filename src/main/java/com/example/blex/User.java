2e7659a9a875121877974423db42b92fd6fa69bc
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-16 11:13:43 +0100

    add User entity

---
+++ src/main/java/com/example/blex/User.java
@@ -0,0 +1,47 @@
+package com.example.blex;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    Long userId;
+    String userName;
+    String name;
+    String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
