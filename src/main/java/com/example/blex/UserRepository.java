c338ba7866f0da1c81ea034297489a9f771ae557
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-16 11:50:44 +0100

    fix variable order

--- src/main/java/com/example/blex/UserRepository.java
+++ src/main/java/com/example/blex/UserRepository.java
@@ -2,5 +2,5 @@ package com.example.blex;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
