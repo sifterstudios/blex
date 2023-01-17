130b11927af0237936d31af0167085f74933bdf7
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-17 08:54:43 +0100

    Add findByUserName()

--- src/main/java/com/example/blex/UserRepository.java
+++ src/main/java/com/example/blex/UserRepository.java
@@ -3,4 +3,5 @@ package com.example.blex;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserName(String username);
 }
