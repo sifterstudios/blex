bee03dfc53705b27e911e00a80a56fe075a160a6
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-18 13:16:03 +0100

    add Header, update render to React@18, add require for typescript to function

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,11 +1,14 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
+const React = require('react');
+import {Component} from "react";
+import Header from "../../../frontend/src/components/Header";
+import {createRoot} from "react-dom/client";
 
 export class App extends Component {
     render() {
         return (
             <div>
                 <h1>B L E X</h1>
+                <Header headerText={'BLEXFLEX'}></Header>
             </div>
         );
     }
@@ -13,6 +16,7 @@ export class App extends Component {
 
 export default App;
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById("app"));
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(<App/>);
