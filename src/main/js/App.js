a743fea4d096751e9a389dc60071de966b45bd20
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-01-18 13:18:24 +0100

    Deleted old react app and settings

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {Button} from "flowbite-react";
+import '../resources/static/style.css'
 
 export class App extends Component {
     render() {
         return (
             <div>
                 <h1>B L E X</h1>
+                <h1 className="bg-red-900 text-white">Hello world</h1>
+                <Button>TEST BUTTON</Button>
             </div>
         );
     }
