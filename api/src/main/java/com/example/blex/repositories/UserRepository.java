f5c5c57048b5e886b7ffa97d67192588ba753d7f
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-19 10:42:53 +0100

    match new user table/entity

--- api/src/main/java/com/example/blex/repositories/UserRepository.java
+++ api/src/main/java/com/example/blex/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.blex.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String username);
+    User findByUsername(String username);
 }
