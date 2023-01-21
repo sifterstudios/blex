6e7066af885d6d998e4776424732c6a7904b873a
bjorneileras <26852670+bjorneileras@users.noreply.github.com>
2023-01-21 08:41:23 +0100

    add RoleRepository

---
+++ api/src/main/java/com/example/blex/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.blex.repositories;
+
+import com.example.blex.entities.Role;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository {
+    Optional<Role> findByRole(String role);
+}
