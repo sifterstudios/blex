1b2dd32964fa7445deb064373e6bdd5d5d4c2810
bjorneileras <26852670+bjorneileras@users.noreply.github.com>
2023-01-21 08:32:10 +0100

    add entity "Role".

---
+++ api/src/main/java/com/example/blex/entities/Role.java
@@ -0,0 +1,32 @@
+package com.example.blex.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
