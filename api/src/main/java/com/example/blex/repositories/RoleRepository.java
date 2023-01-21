db8305bc09976ec851cd360e1b4c51f97a8865c0
bjorneileras <26852670+bjorneileras@users.noreply.github.com>
2023-01-21 10:41:34 +0100

    Fix: add correct inheritance to RoleRepository

--- api/src/main/java/com/example/blex/repositories/RoleRepository.java
+++ api/src/main/java/com/example/blex/repositories/RoleRepository.java
@@ -1,11 +1,12 @@
 package com.example.blex.repositories;
 
 import com.example.blex.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findOptionalByRole(String role);
 }
