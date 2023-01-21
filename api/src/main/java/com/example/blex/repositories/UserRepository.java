b79c140488d9a3ba91a799bb2269697737ab4256
bjorneileras <26852670+bjorneileras@users.noreply.github.com>
2023-01-21 09:15:51 +0100

    update UserRepository. add CustomUserDetailsService

--- api/src/main/java/com/example/blex/repositories/UserRepository.java
+++ api/src/main/java/com/example/blex/repositories/UserRepository.java
@@ -2,7 +2,18 @@ package com.example.blex.repositories;
 
 import com.example.blex.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String password);
+
+    Optional<User> findOptionalByUsername(String username);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
---
+++ api/src/main/java/com/example/blex/security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.example.blex.security;
+
+import com.example.blex.entities.Role;
+import com.example.blex.entities.User;
+import com.example.blex.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findOptionalByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return new org.springframework.security.core.userdetails
+                .User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+    }
+
+}
