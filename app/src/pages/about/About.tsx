d52ea4879057ee72be36b5bda9cf804a96bab287
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-24 17:27:31 +0100

    add content and style

--- app/src/pages/about/About.tsx
+++ app/src/pages/about/About.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">About</h1>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Blex is awesome.</p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Trust us.</p>
+        </div>
     )
 }
