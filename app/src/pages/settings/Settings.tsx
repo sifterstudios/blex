68bb4e2a2328ca696d3a7839f9057cbf81b4de10
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-24 15:37:03 +0100

    try to get user info

--- app/src/pages/settings/Settings.tsx
+++ app/src/pages/settings/Settings.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import axios from "axios";
+import {getCurrentUser} from "../../services/AuthService";
+import authHeader from "../../services/Auth-header";
 
 export const Settings = () => {
+
+  const API_URL = "http://localhost:8080/";
+//TODO:get user info from server....
+  const getPublicContent = async () => {
+      return axios.get(API_URL + "user/"+ getCurrentUser());
+    }
   return (
     <div>Settings</div>
   )
