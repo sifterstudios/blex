4b1e05481688265fd0635d66be84c3f962a17ebf
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-24 13:08:56 +0100

    decode token and return username

--- app/src/services/Auth.service.tsx
+++ app/src/services/Auth.service.tsx
@@ -5,12 +5,19 @@ export const logoutBackEnd = () => {
 };
 
 export const getCurrentUser = () => {
-    const userStr = localStorage.getItem("user");
-    if (userStr) {
-        console.log("userStr: " + userStr);
-        return JSON.parse(userStr);
+    const token = localStorage.getItem("user");
+    if (token) {
+        //JWT uses base64url, so using only atob (which uses base64) isn't enough.
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        const decodedToken = JSON.parse(jsonPayload);
+        console.log("Username: " + decodedToken.sub);
+        return decodedToken.sub;
+
     }
-
-
     return null;
 };
+
