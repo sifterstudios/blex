e8c5fe82015122f1b424bb0f79cb1ede131c742e
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-25 09:41:29 +0100

    Override to remove warnings

--- api/src/main/java/com/example/blex/entities/idkeyclasses/DocumentDownloadsKey.java
+++ api/src/main/java/com/example/blex/entities/idkeyclasses/DocumentDownloadsKey.java
@@ -2,6 +2,7 @@ package com.example.blex.entities.idkeyclasses;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class DocumentDownloadsKey implements Serializable {
@@ -10,6 +11,22 @@ public class DocumentDownloadsKey implements Serializable {
     private Long document_id;
 
     public DocumentDownloadsKey() {
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DocumentDownloadsKey that = (DocumentDownloadsKey) o;
+
+        if (!Objects.equals(user_id, that.user_id)) return false;
+        return Objects.equals(document_id, that.document_id);
+    }
+    @Override
+    public int hashCode() {
+        int result = user_id != null ? user_id.hashCode() : 0;
+        result = 31 * result + (document_id != null ? document_id.hashCode() : 0);
+        return result;
     }
 
 }
