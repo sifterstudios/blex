611e83ac1d22415df08c05f31cc328bbd67281d1
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-25 12:58:30 +0100

    try to add tooltip

--- app/src/components/AddButton/AddButton.tsx
+++ app/src/components/AddButton/AddButton.tsx
@@ -3,7 +3,7 @@ import './AddButton.css'
 export const AddButton = () => {
 	return (
 		<>
-      <div className="w-auto h-auto mr-6">
+      <div className="w-auto h-auto mr-6" data-tooltip-target="tooltip-bottom" data-tooltip-placement="bottom">
         <div className="flex-1 h-full">
           <div className="flex items-center justify-center flex-1 h-full p-2 bg-pink-600 text-white  rounded-full
           shadow-[0_4px_0_rgb(0,0,0)] ease-out ">
@@ -15,6 +15,11 @@ export const AddButton = () => {
           </div>
         </div>
       </div>
+            <div id="tooltip-bottom" role="tooltip"
+                 className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Tooltip on bottom
+                <div className="tooltip-arrow" data-popper-arrow></div>
+            </div>
 		</>
 	 )
 }
