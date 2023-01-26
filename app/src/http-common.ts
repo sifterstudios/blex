f8625c0b50980c70439816471477210aa3cc4ed8
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-01-26 05:04:27 +0100

    Hardcoded API base url

--- app/src/http-common.ts
+++ app/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: process.env.API_BASE_URL,
+	baseURL: 'http://api.blex.today',
 	headers: {
 		"Content-type": "application/json",
 	},
