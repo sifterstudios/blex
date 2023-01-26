7643924ffd9f204cab2252675302df09ff531fcb
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-01-26 05:36:13 +0100

    Changed api path to https

--- app/src/http-common.ts
+++ app/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: 'http://api.blex.today',
+	baseURL: 'https://api.blex.today',
 	headers: {
 		"Content-type": "application/json",
 	},
