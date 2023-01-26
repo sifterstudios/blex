67a764b47998cbb384b95c8fe0525474ac266477
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-26 11:18:08 +0100

    search functionality

--- app/src/pages/search/Search.tsx
+++ app/src/pages/search/Search.tsx
@@ -1,7 +1,41 @@
 import { TextInput, Label, Button } from 'flowbite-react'
-import React from 'react'
+import React, {useEffect} from 'react'
+import http from "../../http-common";
 
 export const Search = () => {
+  const [query, setQuery] = React.useState('')
+  const [results, setResults] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [message, setMessage] = React.useState('')
+    const [error, setError] = React.useState(false)
+
+    useEffect(() => {
+    const handleSearch = async () => {
+        setLoading(true)
+        setMessage('')
+        setError(false)
+
+        try {
+            const response = await http.get('?????/'+{query})
+            //const { items } = await response.json()
+            if (!items.length) {
+                setMessage('No search results. Please try again.')
+            }
+            setResults(items)
+        } catch (error) {
+            setError(true)
+        }
+        setLoading(false)
+    }
+    }
+    const handleOnInputChange = (query:string) => {
+        setQuery(query);
+
+        setMessage('');
+        setResults([]);
+        setError(false);
+    }
+
   return (
 <div className="flex h-96 flex-col justify-center gap-4 p-56">
   <TextInput
@@ -10,6 +44,7 @@ export const Search = () => {
     placeholder="Search for blex!"
     required={true}
     sizing="lg"
+    onChange={(e) => handleOnInputChange(e.target.value)}
   />
 <Button>Search</Button>
 </div>
