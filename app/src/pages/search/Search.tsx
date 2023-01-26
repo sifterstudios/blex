da6690d3af6b3d490f51427733ddeb051ecc3ac7
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-26 21:17:37 +0100

    search yields result in table

--- app/src/pages/search/Search.tsx
+++ app/src/pages/search/Search.tsx
@@ -2,7 +2,7 @@ import { Table } from 'flowbite-react';
 import pdf from '../../types/pdf';
 import axios from 'axios';
 import { TextInput, Label, Button } from 'flowbite-react'
-import React, {useEffect} from 'react'
+import React from 'react'
 
 interface Blekk {
   id: number;
@@ -11,10 +11,6 @@ interface Blekk {
   type: string;
   filename: string;
 }
-
-
-
-
 
 export const Search = () => {
 
@@ -32,7 +28,6 @@ export const Search = () => {
 
   const [query, setQuery] = React.useState('')
   const [results, setResults] = React.useState<Blekk[]>([])
-  const [loading, setLoading] = React.useState(false)
   const [message, setMessage] = React.useState('')
   const [error, setError] = React.useState(false)
   const cache = localStorage.getItem('pdfList');
@@ -40,7 +35,7 @@ export const Search = () => {
 
     //TODO search on change - and add match on partial word
     //TODO fix superbuggy search - it works but not as intended, returns only one result. Keeps old result if any match on new search
-    const log = (query:any) => {
+    const log = () => {
         setResults([]);
         console.log(results)
         console.log("Query: " + query);
@@ -62,71 +57,63 @@ export const Search = () => {
                 setResults([...results, ...captured] )
             }
         }
-
-    }
     if (results.length == 0) {
       console.log("No results found")
     }
     console.log("ResultArray: "+results);
-  
-
-
-
-
-
-
-
+    }
 
   return (
-<div className="flex h-96 flex-col justify-center gap-4 p-56">
-  <TextInput
-      id="searchText"
-      type="text"
-      placeholder="Search for blex!"
-      required={true}
-      sizing="lg"
-      onChange={(e) => setQuery(e.target.value)}
-  />
-  <Button onClick={()=>log(query)}>Search</Button>
-
-<Table
-				striped={true}
-				hoverable={true}
-			>
-				<Table.Head>
-					<Table.HeadCell>
-						Song
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Artist
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Type
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Rating
-					</Table.HeadCell>
-				</Table.Head>
-				<Table.Body className="divide-y">
-					{pdfs.map(pdf => {
-						return (
-							<Table.Row key={pdf.id} onClick={() => handleRowClick(pdf.id)} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-								<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-									{pdf.songtitle}
-								</Table.Cell>
-								<Table.Cell>
-									{pdf.artist}
-								</Table.Cell>
-								<Table.Cell>
-									{pdf.type}
-								</Table.Cell>
-								<Table.Cell>
-									stars here
-								</Table.Cell>
-							</Table.Row>
-					)})}
-				</Table.Body>
-			</Table>
-</div>
-  )
-}
+      <>
+    <div className="flex h-96 flex-col justify-center gap-4 p-56">
+          <TextInput
+              id="searchText"
+              type="text"
+              placeholder="Search for blex!"
+              required={true}
+              sizing="lg"
+              onChange={(e) => setQuery(e.target.value)}
+          />
+          <Button onClick={log}>Search</Button>
+        </div>
+        <Table
+            striped={true}
+            hoverable={true}
+        >
+            <Table.Head>
+                <Table.HeadCell>
+                            Song
+                </Table.HeadCell>
+                <Table.HeadCell>
+                            Artist
+                </Table.HeadCell>
+                <Table.HeadCell>
+                            Type
+                </Table.HeadCell>
+                <Table.HeadCell>
+                            Rating
+                </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+                        {results.map(result => {
+                            return (
+                                <Table.Row key={result.id} onClick={() => handleRowClick(result.id)} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                        {result.songtitle}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {result.artist}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {result.type}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        stars here
+                                    </Table.Cell>
+                                </Table.Row>
+                        )})}
+                    </Table.Body>
+                </Table>
+    </>
+      )
+    }
