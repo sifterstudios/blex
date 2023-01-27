c14f954b6887f826a5a0ae866074b2868fc10f8c
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-27 10:51:08 +0100

    add white text on darkmode

--- app/src/components/BlexPdfViewer/BlexPdfViewer.tsx
+++ app/src/components/BlexPdfViewer/BlexPdfViewer.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import {useEffect} from "react";
 import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
-import axios from "axios";
-import pdf from "../../types/pdf";
 
 
 
@@ -15,22 +12,24 @@ export const BlexPdfViewer: React.FC<Props> = ({id}) => {
 	//const filepath:string = 'https://api.blex.today/document/download/';
 	const filepath:string = 'http://localhost:8080/document/download/';
 
-interface pages {
-	numPages : number;
-}
+	interface pages {
+		numPages : number;
+	}
 
 	function onDocumentLoadSuccess({ numPages }: pages) {
 		setNumPages(numPages);
-}
+	}
 
 	return (
 		<>
-			<Document file={filepath+id} onLoadSuccess={onDocumentLoadSuccess}>
-				<Page pageNumber={pageNumber} />
-			</Document>
-			<p>
-				Page {pageNumber} of {numPages}
-			</p>
-		</>)
+			<div className="dark:text-white">
+				<Document file={filepath+id} onLoadSuccess={onDocumentLoadSuccess} className="dark:text-white">
+					<Page pageNumber={pageNumber} className="dark:text-white"/>
+				</Document>
+				<p className="dark:text-white">
+					Page {pageNumber} of {numPages}
+				</p>
+			</div>
+		</>
+	)
 }
-
